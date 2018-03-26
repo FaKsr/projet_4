89699bf761682e76b94c7182f5aa6e654a5ccb83
Fkers <fanny.kersuzan@gmail.com>
2018-03-26 14:04:35 +0200

    Gestion d'erreur
    ajout du bloc try & catch

--- index.php
+++ index.php
@@ -3,43 +3,33 @@
 // On charge le controller, pour que les fonctions soient en mémoire
 require('controller/frontend.php');
 
-if (isset($_GET['action'])) 
-{
-    if ($_GET['action'] == 'listPosts') 
-    {
-        listPosts();
-    } 
-    elseif ($_GET['action'] == 'post')
-     {
-        if (isset($_GET['id']) && $_GET['id'] > 0) 
-        {
-            post();
-        } 
-        else 
-        {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-        }
-    } 
-    elseif ($_GET['action'] == 'addComment') 
-    {
-        if (isset($_GET['id']) && $_GET['id'] > 0) 
-        {
-            if (!empty($_POST['author']) && !empty($_POST['comment'])) 
-            {
-                addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-            } 
-            else 
-            {
-                echo ' Erreur : tous les champs ne sont pas remplis ! ';
+try {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        } elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            } else {
+                throw new Exception(' Aucun identifiant de billet envoyé'); //stop le bloc try pour aller directement au catch
+            }
+        } elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                } else {
+                    throw new Exception( 'Tous les champs ne sont pas remplis !');
+                }
+            } else {
+                throw new Exception(' Aucun identifiant de billet envoyé');
             }
         }
-        else 
-        {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-        }
+    } else {
+        listPosts();
     }
-} 
-else 
+}
+//récupère le message d'erreur transmis et affiche le message
+catch(Exception $e)
 {
-    listPosts();
+    echo 'Erreur : ' .$e->getMessage();
 }
